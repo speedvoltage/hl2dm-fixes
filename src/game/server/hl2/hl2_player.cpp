0a2450b53e0c0163d697ac17a47968a8852736ed
speedvoltage <32344549+speedvoltage@users.noreply.github.com>
2026-08-25 12:31:47 -0700

    Respect targeted physics object drops
    Only clear hand-held or physcannon-held objects when they match the requested entity.

--- src/game/server/hl2/hl2_player.cpp
+++ src/game/server/hl2/hl2_player.cpp
@@ -3213,10 +3213,13 @@ void CHL2_Player::ForceDropOfCarriedPhysObjects( CBaseEntity *pOnlyIfHoldingThis
 #endif
 
 	// Drop any objects being handheld.
-	ClearUseEntity();
+	if ( pOnlyIfHoldingThis == NULL || IsHoldingEntity( pOnlyIfHoldingThis ) )
+	{
+		ClearUseEntity();
+	}
 
 	// Then force the physcannon to drop anything it's holding, if it's our active weapon
-	PhysCannonForceDrop( GetActiveWeapon(), NULL );
+	PhysCannonForceDrop( GetActiveWeapon(), pOnlyIfHoldingThis );
 }
 
 void CHL2_Player::InputForceDropPhysObjects( inputdata_t &data )
