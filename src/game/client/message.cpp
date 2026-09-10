2f72d72a89add621ece879b594789c7f707e5453
speedvoltage <32344549+speedvoltage@users.noreply.github.com>
2026-09-10 00:12:17 -0700

    [ALL] Measure HUD messages with the selected font
    Issue: HUD messages using custom fonts are measured with the default font.
    Fix: Select the requested font before calculating text positions and backdrop dimensions.
    
    Source: https://github.com/FractureENT/MapbaseSourceSDK-MP/commit/8d6bf01b791752abe8505af0476eaca9a7760156
    
    Co-authored-by: samisalreadytaken <46823719+samisalreadytaken@users.noreply.github.com>

--- src/game/client/message.cpp
+++ src/game/client/message.cpp
@@ -446,15 +446,6 @@ void CHudMessage::MessageScanStart( void )
 			m_parms.fadeBlend = 0;
 		break;
 	}
-
-	m_parms.font = g_hFontTrebuchet24;
-
-	if ( m_parms.vguiFontName != NULL && 
-		m_parms.vguiFontName[ 0 ] )
-	{
-
-		SetFont( vgui::scheme()->GetDefaultScheme(), m_parms.vguiFontName );
-	}
 }
 
 //-----------------------------------------------------------------------------
@@ -498,6 +489,10 @@ void CHudMessage::MessageDrawScan( client_textmessage_t *pMessage, float time )
 	m_parms.vguiFontName = pMessage->pVGuiSchemeFontName;
 
 	m_parms.font = g_hFontTrebuchet24;
+	if ( m_parms.vguiFontName != NULL && m_parms.vguiFontName[0] )
+	{
+		SetFont( vgui::scheme()->GetDefaultScheme(), m_parms.vguiFontName );
+	}
 
 	while ( *pText )
 	{
