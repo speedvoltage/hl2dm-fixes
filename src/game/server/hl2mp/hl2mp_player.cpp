80546097264c16d32fd1a067bff481ed4c6f6f10
speedvoltage <32344549+speedvoltage@users.noreply.github.com>
2026-09-10 01:13:26 -0700

    [HL2MP] Correct predicted stunstick activation sounds
    Issue: Stunstick sounds can replay during prediction, use the wrong emitting entity, or play when a remote weapon leaves PVS.
    
    Fix: Use the owner as the sound emitter with prediction-aware recipients, suppress dormant remote-client playback, and grant the gravity gun first to avoid an unnecessary spawn-time stunstick deploy.
    
    Origin: Third-party community fix.
    Source-PR: https://github.com/ValveSoftware/source-sdk-2013/pull/2045
    Upstream-commit: ec354494b5c2224614733028490473c8bff45d07
    (cherry picked from commit b5d4c92233400d051f91f933b2db32d58e41910b)
    
    Co-authored-by: Brandon Little <blittle@inco.cc>

--- src/game/server/hl2mp/hl2mp_player.cpp
+++ src/game/server/hl2mp/hl2mp_player.cpp
@@ -227,6 +227,8 @@ void CHL2MP_Player::GiveAllItems( void )
 	CBasePlayer::GiveAmmo( 1,	"grenade" );
 	CBasePlayer::GiveAmmo( 2,	"slam" );
 
+	GiveNamedItem( "weapon_physcannon" );
+
 	GiveNamedItem( "weapon_crowbar" );
 	GiveNamedItem( "weapon_stunstick" );
 	GiveNamedItem( "weapon_pistol" );
@@ -243,8 +245,6 @@ void CHL2MP_Player::GiveAllItems( void )
 	GiveNamedItem( "weapon_rpg" );
 
 	GiveNamedItem( "weapon_slam" );
-
-	GiveNamedItem( "weapon_physcannon" );
 	
 }
 
@@ -258,6 +258,8 @@ void CHL2MP_Player::GiveDefaultItems( void )
 	CBasePlayer::GiveAmmo( 6,	"Buckshot");
 	CBasePlayer::GiveAmmo( 6,	"357" );
 
+	GiveNamedItem( "weapon_physcannon" );
+
 	if ( GetPlayerModelType() == PLAYER_SOUNDS_METROPOLICE || GetPlayerModelType() == PLAYER_SOUNDS_COMBINESOLDIER )
 	{
 		GiveNamedItem( "weapon_stunstick" );
@@ -270,7 +272,6 @@ void CHL2MP_Player::GiveDefaultItems( void )
 	GiveNamedItem( "weapon_pistol" );
 	GiveNamedItem( "weapon_smg1" );
 	GiveNamedItem( "weapon_frag" );
-	GiveNamedItem( "weapon_physcannon" );
 
 	const char *szDefaultWeaponName = engine->GetClientConVarValue( engine->IndexOfEdict( edict() ), "cl_defaultweapon" );
 
--- src/game/shared/hl2mp/weapon_stunstick.cpp
+++ src/game/shared/hl2mp/weapon_stunstick.cpp
@@ -184,8 +184,6 @@ IMPLEMENT_ACTTABLE(CWeaponStunStick);
 //-----------------------------------------------------------------------------
 CWeaponStunStick::CWeaponStunStick( void )
 {
-	// HACK:  Don't call SetStunState because this tried to Emit a sound before
-	//  any players are connected which is a bug
 	m_bActive = false;
 
 #ifdef CLIENT_DLL
@@ -442,12 +440,22 @@ void CWeaponStunStick::SetStunState( bool state )
 		data.m_nEntIndex = entindex();
 #endif
 		DispatchEffect( "StunstickActivate", data );
-
-		EmitSound( "Weapon_StunStick.Activate" );
 	}
-	else
+
+#ifdef CLIENT_DLL
+	// Workaround to prevent sounds from playing on other players when they leave PVS.
+	if ( GetPredictable() )
+#endif
 	{
-		EmitSound( "Weapon_StunStick.Deactivate" );
+		CBaseEntity *pEntity = GetOwner() != NULL ? GetOwnerEntity() : this;
+		const char *sound = m_bActive ? "Weapon_StunStick.Activate" : "Weapon_StunStick.Deactivate";
+
+		CPASAttenuationFilter filter( pEntity, sound );
+
+		if ( IsPredicted() && CBaseEntity::GetPredictionPlayer() )
+			filter.UsePredictionRules();
+
+		EmitSound( filter, pEntity->entindex(), sound );
 	}
 }
 
